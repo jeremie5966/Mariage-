3290b5247eaa75446ce8102565d75de78cf13e0a
Your Name <you@example.com>
2026-08-22 15:21:10 +0200

    Restore API path in Vercel entrypoint

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,3 +1,14 @@
 <?php
 
+if (isset($_GET['path']) && is_string($_GET['path'])) {
+	$path = '/'.ltrim($_GET['path'], '/');
+
+	if (!str_starts_with($path, '/api/')) {
+		$path = '/api'.$path;
+	}
+
+	$_SERVER['REQUEST_URI'] = $path;
+	$_SERVER['PATH_INFO'] = $path;
+}
+
 require __DIR__.'/../public/index.php';
