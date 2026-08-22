b7bd0ea11219bce5b3ee561165b20c9582eb22d1
Your Name <you@example.com>
2026-08-22 15:24:05 +0200

    Point mobile API to working Vercel alias

--- mobile/lib/core/config/app_config.dart
+++ mobile/lib/core/config/app_config.dart
@@ -22,8 +22,8 @@ class AppConfig {
     };
 
     final defaultUrl = switch (environment) {
-        AppEnvironment.production =>
-          'https://mariage-tct2.vercel.app/api/index.php/api',
+      AppEnvironment.production =>
+        'https://mariage-tct2-jeremie5966s-projects.vercel.app/api/index.php/api',
       AppEnvironment.staging => 'https://staging-api.mon-domaine.com/api',
       AppEnvironment.development => 'http://11.11.11.244:8000/api',
     };
